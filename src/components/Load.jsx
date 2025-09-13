80e78e2419933588ec982b16dcaf1e71ebee2bca
Fernando Gustavo <fernando.gdev@gmail.com>
2025-09-13 15:57:32 -0300

    implementamdo telas: adm, login, users

--- src/components/Load.jsx
+++ src/components/Load.jsx
@@ -1,6 +1,6 @@
 // components/Loader.js
 import React from "react";
-import "./Load.css"; // estilos do spinner
+import "./load.css"; // estilos do spinner
 
 export default function Load() {
   return (
